220a6208ccb7a99f74b827695bfe914214a2c11c
Ejokpe Ejeguo <ejeguo.ejokpe@hotmail.com>
2021-11-06 19:10:51 +0100

    complete development and containerization of catalog API using docker

---
+++ src/Services/Catalog/Catalog.API/Startup.DI.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.DataAccess;
+using Catalog.API.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.API
+{
+    public partial class Startup
+    {
+        public IServiceCollection ConfigureDIServices(IServiceCollection services)
+        {
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();            
+            return services;
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,19 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.API
 {
-    public class Startup
+    public partial class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -25,7 +19,7 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            ConfigureDIServices(services);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
