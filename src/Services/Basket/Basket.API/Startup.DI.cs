91e5bc805acfdf9574f92658bd74de882c648b49
Ejokpe Ejeguo <ejeguo.ejokpe@hotmail.com>
2021-11-06 22:07:24 +0100

    Basket microservice developed

---
+++ src/Services/Basket/Basket.API/Startup.DI.cs
@@ -0,0 +1,14 @@
+﻿using Basket.API.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket.API
+{
+    public partial class Startup
+    {
+        public IServiceCollection ConfigureDIServices(IServiceCollection services)
+        {
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            return services;
+        }
+    }
+}
